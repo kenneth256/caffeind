3f0b3ff2576c126f8527835736bda145f84dc6a3
kenneth david <kennethdavid256.com>
2024-11-22 08:57:34 +0300

    updated config

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,21 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: [
-            'react',
-            'react-dom',
-          ],
-        }
-      }
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
     },
-    chunkSizeWarningLimit: 1000,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true
   }
 })
